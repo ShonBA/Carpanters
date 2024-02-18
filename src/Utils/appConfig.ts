958a09529dc6b9827e3e6d8611665e7d04e0f293
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-18 15:05:08 +0200

    changed spelling on appconfig

--- src/Utils/appConfig.ts
+++ src/Utils/appConfig.ts
@@ -3,9 +3,9 @@ class AppConfig {
     public readonly HomeRoute: string = "/home";
     public readonly GalleryRoute: string = "/gallery";
     public readonly ProjectsRoute: string = "/projects";
-    public readonly CustomCarpenterRoute: string = "/custom-carpenter";
+    public readonly CustomCarpenterRoute: string = "/custom-carpentry";
     public readonly AboutRoute: string = "/about";
-    public readonly RecommendationRoute: string = "/recommendation";
+    public readonly RecommendationRoute: string = "/recommendations";
     public readonly ContactUsRoute: string = "/contact-us";
 
 }
