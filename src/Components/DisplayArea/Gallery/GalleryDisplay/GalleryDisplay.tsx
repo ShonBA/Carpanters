2913ef16863a5fbbe797c87897ea33f5887b811a
Shon Benayoun <shonba98@gmail.com>
2024-02-26 16:30:51 +0200

    style form btn and add collapse gallery

--- src/Components/DisplayArea/Gallery/GalleryDisplay/GalleryDisplay.tsx
+++ src/Components/DisplayArea/Gallery/GalleryDisplay/GalleryDisplay.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { Collapse } from 'react-collapse';
 import { NavLink } from "react-router-dom";
 import DataArea from "../../../../Service/DataArea";
 import GalleryCard from "../GalleryCard/GalleryCard";
@@ -7,19 +9,27 @@ function GalleryDisplay(): JSX.Element {
     const galleryData = DataArea.galleryData;
     const materials = ["פורמיקה", "מלמין", "פולימר"];
 
+    const [isOpen, setIsOpen] = useState<boolean>(false);
     return (
         <div className="GalleryDisplay">
-            {materials.map((material, index) => (
-                <div key={index}>
-                    <h2>מטבחי {material} <NavLink to={'#'}>ראה עוד...</NavLink></h2>
-                    <div className="gallery__row">
-                        {galleryData
-                            .filter(g => g.material === material)
-                            .map(d => <GalleryCard key={d.id} gallery={d} />)
-                        }
+            <h2>מטבחים 
+                {isOpen ?
+                <a onClick={() => setIsOpen(false)}>סגור</a> :
+                <a onClick={() => setIsOpen(true)}>ראה עוד...</a>}
+            </h2>
+            <Collapse isOpened={isOpen}>
+                {materials.map((material, index) => (
+                    <div key={index}>
+                        <h2>מטבחי {material} <NavLink to={'#'}>ראה עוד מטבחי {material}..</NavLink></h2>
+                        <div className="gallery__row">
+                            {galleryData
+                                .filter(g => g.material === material)
+                                .map(d => <GalleryCard key={d.id} gallery={d} />)
+                            }
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </Collapse>
         </div>
     );
 }
