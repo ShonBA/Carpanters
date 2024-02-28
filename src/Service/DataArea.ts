dbe603fb3f492376cf10e9485fc545ff82aa0157
Shon Benayoun <shonba98@gmail.com>
2024-02-28 22:18:41 +0200

    redux complete

--- src/Service/DataArea.ts
+++ src/Service/DataArea.ts
@@ -4,26 +4,47 @@ import ProjectModel from "../Models/ProjectModel";
 import QualityModel from "../Models/QualityModel";
 import RecommendationModel from "../Models/RecommendationModel";
 import appConfig from "../Utils/appConfig";
+import { DataAction, DataActionTypes, dataStore } from "../Redux/DataState";
 
 class DataService {
     public async getAllAboutUsData(): Promise<QualityModel[]> {
-        const response = await axios.get<QualityModel[]>(appConfig.aboutUsUrl);
-        const data = response.data;
+        let data = dataStore.getState().aboutUs
+        if (data.length > 0) {
+            const response = await axios.get<QualityModel[]>(appConfig.aboutUsUrl);
+            data = response.data;
+        }
+        const action: DataAction = { type: DataActionTypes.SetAboutUs, payload: data };
+        dataStore.dispatch(action)
         return data;
     }
     public async getAllProjectsData(): Promise<ProjectModel[]> {
-        const response = await axios.get<ProjectModel[]>(appConfig.projectsUrl);
-        const data = response.data;
+        let data = dataStore.getState().projects
+        if (data.length > 0) {
+            const response = await axios.get<ProjectModel[]>(appConfig.projectsUrl);
+            data = response.data;
+        }
+        const action: DataAction = { type: DataActionTypes.SetProjects, payload: data };
+        dataStore.dispatch(action)
         return data;
     }
     public async getAllRecommendationsData(): Promise<RecommendationModel[]> {
-        const response = await axios.get<RecommendationModel[]>(appConfig.recommendationsUrl);
-        const data = response.data;
+        let data = dataStore.getState().Recommendations
+        if (data.length > 0) {
+            const response = await axios.get<RecommendationModel[]>(appConfig.recommendationsUrl);
+            data = response.data;
+        }
+        const action: DataAction = { type: DataActionTypes.SetRecommendations, payload: data };
+        dataStore.dispatch(action)
         return data;
     }
     public async getAllGalleryData(): Promise<GalleryCategoryModel[]> {
-        const response = await axios.get<GalleryCategoryModel[]>(appConfig.galleryUrl);
-        const data = response.data;
+        let data = dataStore.getState().galleries
+        if (data.length > 0) {
+            const response = await axios.get<GalleryCategoryModel[]>(appConfig.galleryUrl);
+            data = response.data;
+        }
+        const action: DataAction = { type: DataActionTypes.SetGalleries, payload: data };
+        dataStore.dispatch(action)
         return data;
     }
 }
