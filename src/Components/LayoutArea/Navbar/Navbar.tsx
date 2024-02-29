24c287d2af0fc4b03eefa34a55ba9b055f11ce53
Alexander Vasilenko <alexandervjr1@gmail.com>
2024-02-29 09:36:31 +0200

    rotated navbar chevron

--- src/Components/LayoutArea/Navbar/Navbar.tsx
+++ src/Components/LayoutArea/Navbar/Navbar.tsx
@@ -7,8 +7,14 @@ function Navbar(): JSX.Element {
     return (
         <div className="Navbar">
             <label className="container">
-                <input type="checkbox"/>
-                <svg viewBox="0 0 320 512" height="1em" xmlns="http://www.w3.org/2000/svg" className="chevron-right"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"></path></svg>
+                <input type="checkbox" />
+                <svg
+                    viewBox="0 0 320 512"
+                    height="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="chevron-right">
+                    <path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l192-192c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3L77.3 256l169.3 169.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0l-192-192z"></path>
+                </svg>
             </label>
             <ul>
                 <li><NavLink to={appConfig.HomeRoute} className="navbar-logo"><img src={logo} alt="logo" /></NavLink></li>
