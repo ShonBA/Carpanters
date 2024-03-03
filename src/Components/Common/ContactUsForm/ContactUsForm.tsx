8f2aac0ad94f53514c5f93877209793105f74ddd
Shon Benayoun <shonba98@gmail.com>
2024-03-03 16:54:20 +0200

    add social icons

--- src/Components/Common/ContactUsForm/ContactUsForm.tsx
+++ src/Components/Common/ContactUsForm/ContactUsForm.tsx
@@ -1,4 +1,7 @@
 import { useForm } from '@formspree/react';
+import { CiFacebook, CiInstagram } from "react-icons/ci";
+import { FaWhatsapp } from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
 import "./ContactUsForm.scss";
 
 
@@ -9,20 +12,29 @@ function ContactUsForm(): JSX.Element {
         return <p>Thanks for joining!</p>;
     }
     return (
-        <div className='ContactUsForm'>
-            <form onSubmit={handleSubmit} className='formContainer'>
-                <div className='form-data'>
-                    <input type="text" name="Full Name" placeholder='שם מלא' />
-                    <input type="email" name="Email" placeholder='דוא"ל' />
-                    <input type="tel" name="Phone" placeholder='טלפון' />
-                    <button type="submit" disabled={state.submitting}>שלח</button>
-                </div>
-                <div className='form-content'>
-                    <h2>דברו איתנו!</h2>
-                    <p>אנו זמינים עבורכם לכל שאלה. השאירו פרטים ואנו מבטיחים לחזור אליכם תוך 2 ימי עסקים.</p>
-                </div>
-            </form>
-        </div>
+        <form onSubmit={handleSubmit} className='ContactUsForm'>
+            <div className='form-data'>
+                <input type="text" name="Full Name" placeholder='שם מלא' />
+                <input type="email" name="Email" placeholder='דוא"ל' />
+                <input type="tel" name="Phone" placeholder='טלפון' />
+                <button type="submit" disabled={state.submitting}>שלח</button>
+            </div>
+            <div className='form-content'>
+                <h2>דברו איתנו!</h2>
+                <p>אנו זמינים עבורכם לכל שאלה. השאירו פרטים ואנו מבטיחים לחזור אליכם תוך 2 ימי עסקים.</p>
+            </div>
+            <div className='form-icons'>
+                <NavLink to={"#"}>
+                    <FaWhatsapp />
+                </NavLink>
+                <NavLink to={"#"}>
+                    <CiInstagram />
+                </NavLink>
+                <NavLink to={"#"}>
+                    <CiFacebook />
+                </NavLink>
+            </div>
+        </form>
     );
 }
 
