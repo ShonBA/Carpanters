489a47c0f50078bdb6e436865e466ec32cb0c23f
ShonBA <135617972+ShonBA@users.noreply.github.com>
2024-03-03 14:26:39 +0200

    Update appConfig.ts

--- src/Utils/appConfig.ts
+++ src/Utils/appConfig.ts
@@ -1,10 +1,10 @@
 class AppConfig {
 
     // Axios Urls
-    public readonly aboutUsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersAboutData.json?token=GHSAT0AAAAAACMKEZYGMICIBXZLW3VR62NWZPENJWQ";
-    public readonly projectsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersProjectsData.json?token=GHSAT0AAAAAACMKEZYHSRJOTW4QT2VLRE4KZPENKHQ";
-    public readonly recommendationsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersGalleryData.json?token=GHSAT0AAAAAACMKEZYGXQA3C7BDVUETET5UZPENJ7A";
-    public readonly galleryUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersGalleryData.json?token=GHSAT0AAAAAACMKEZYGXQA3C7BDVUETET5UZPENJ7A";
+    public readonly aboutUsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersAboutData.json";
+    public readonly projectsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersProjectsData.json";
+    public readonly recommendationsUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersGalleryData.json";
+    public readonly galleryUrl: string = "https://raw.githubusercontent.com/ShonBA/Carpanters/main/public/JSON/carpentersGalleryData.json";
 
     // Routes
     public readonly HomeRoute: string = "/home";
