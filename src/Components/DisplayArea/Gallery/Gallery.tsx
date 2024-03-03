2d20e0d20ef4476bca201e3644f70f86a1cedaa3
Shon Benayoun <shonba98@gmail.com>
2024-03-03 22:22:40 +0200

    Shon

--- src/Components/DisplayArea/Gallery/Gallery.tsx
+++ src/Components/DisplayArea/Gallery/Gallery.tsx
@@ -1,15 +1,20 @@
-import useTitle from "../../../Utils/useTitle";
-import GalleryDisplay from "./GalleryDisplay/GalleryDisplay";
-import "./Gallery.scss";
 import x from '../../../Assets/Images/backround_image2.jpg';
+import useTitle from "../../../Utils/useTitle";
+import "./Gallery.scss";
+import GalleryDisplay from "./GalleryDisplay/GalleryDisplay";
+import GalleryMaterials from "./GalleryMaterials/GalleryMaterials";
+
 function Gallery(): JSX.Element {
     useTitle("Gallery");
-
     return (
         <div className="Gallery">
             <div className="galleryHeaderContainer">
                 <img src={x} />
                 <h2>גלריית עבודות</h2>
+            </div>
+            <div className="galleryParaContainer">
+                <h2>עם מה אנחנו עובדים ?</h2>
+                <GalleryMaterials />
             </div>
             <div className="gallery">
                 <GalleryDisplay />
--- src/Models/GalleryCategoryModel.ts
+++ src/Models/GalleryCategoryModel.ts
@@ -3,7 +3,6 @@ import GalleryModel from "./GalleryModel";
 class GalleryCategoryModel {
     public id: number;
     public type: string;
-    public typeShort: string;
     public materials: string[];
     public data: GalleryModel[];
 }
