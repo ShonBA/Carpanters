60341cdb0fa3a851864189fb808f52da03b136d8
Shon Benayoun <shonba98@gmail.com>
2024-03-18 15:03:41 +0200

    start project page

--- src/Components/DisplayArea/ProjectsPage/ProjectsPage.tsx
+++ src/Components/DisplayArea/ProjectsPage/ProjectsPage.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProjectModel from "../../../Models/ProjectModel";
 import dataService from "../../../Service/DataArea";
-import "./ProjectsPage.css";
+import CarouselLib from "react-multi-carousel";
+import "./ProjectsPage.scss";
 
 export function ProjectsPage(): JSX.Element {
 
@@ -14,8 +15,43 @@ export function ProjectsPage(): JSX.Element {
             .then(beProjects => setFeProject(beProjects))
             .catch(err => console.log(err))
     }, [])
+
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    };
+
     return (
         <div className="ProjectsPage">
+            <div className="projectPageHeaderContainer">
+                <h2>{feProject?.description}</h2>
+            </div>
+            <div>
+                <p>עיצוב: {feProject?.designer}</p>
+                <p>צילום: {feProject?.photographer}</p>
+            </div>
+            <div>
+                {feProject && <CarouselLib 
+                responsive={responsive}
+                >
+                    {feProject?.imagesNames.map((image, index) =>
+                        <img key={index} src={require(`../../../Assets/Images/Projects/${feProject?.imageFolderName}${image}`)} alt={image} />
+                    )}
+                </CarouselLib>}
+            </div>
         </div>
     );
 }
