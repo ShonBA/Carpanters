523da5d6b2804228b6c9c6239de23afd8cf97433
Shon Benayoun <shonba98@gmail.com>
2024-03-22 18:14:55 +0200

    fix bug

--- src/Components/PagesArea/ProjectPage/ProjectPage.tsx
+++ src/Components/PagesArea/ProjectPage/ProjectPage.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ProjectModel } from "../../../Models/ProjectModel";
 import dataService from "../../../Service/DataArea";
-import { ProjectPageGallery } from "./ProjectPageGallery/ProjectPageGallery";
 import "./ProjectPage.scss";
-import data from "../../../Service/TestingProjectsData";
+import { ProjectPageGallery } from "./ProjectPageGallery/ProjectPageGallery";
 
 export function ProjectsInfo(): JSX.Element {
 
@@ -12,29 +11,26 @@ export function ProjectsInfo(): JSX.Element {
     const [feProject, setFeProject] = useState<ProjectModel>();
 
     useEffect(() => {
-        // Should be 
-        // dataService.getProjectDataByUuid(params.uuid)
-        //     .then(beProject => setFeProject(beProject))
-        //     .catch(err => console.log(err))
-
-        // Switching this will kill the gallery
-
-        // Should be Deleted!!!
-        const project = data.filter(d => d.uuid === params.uuid)[0];
-        setFeProject(project);
+        dataService.getProjectDataByUuid(params.uuid)
+            .then(beProject => setFeProject(beProject))
+            .catch(err => console.log(err))
     }, [])
+
+    if (!feProject) return <div>Loading...</div>
 
     return (
         <div className="ProjectsInfo">
             <div className="ProjectPageHeaderContainer">
-                <h2>{feProject?.description}</h2>
+                <h2>{feProject.description}</h2>
             </div>
-            <div>
-                <p>עיצוב: {feProject?.designer}</p>
-                <p>צילום: {feProject?.photographer}</p>
-            </div>
-            <div>
-                <ProjectPageGallery project={feProject} />
+            <div className="projectContainer">
+                <div>
+                    <ProjectPageGallery project={feProject} />
+                </div>
+                <div>
+                    <p>עיצוב: {feProject.designer}</p>
+                    <p>צילום: {feProject.photographer}</p>
+                </div>
             </div>
         </div>
     );
